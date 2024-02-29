97bff57c9f1e36ece3a7ecea39214d37830a30e3
HafidOD <haifido1403@gmail.com>
2024-02-28 19:36:47 -0500

    some

--- src/app/api/save-img/route.js
+++ src/app/api/save-img/route.js
@@ -17,48 +17,45 @@ export async function GET(request) {
     );
   }
 }
+
 export async function POST(request) {
-  return NextResponse.json({ imageName: `hola mundo` }, { status: 200 });
-  // return NextResponse.json({ product }, { status: 200 });
+  console.log(request);
+  try {
+    const data = await request.json();
+    // console.log(data);
+    const timestamp = Date.now();
+    const randomString = Math.random().toString(36).substring(2, 18);
+
+    const base64Data = await data.replace(/^data:image\/png;base64,/, "");
+    const buffer = Buffer.from(base64Data, "base64");
+    // fs.writeFileSync(
+    //   `public/images/tar/${timestamp}_${randomString}.png`,
+    //   base64Data,
+    //   "base64"
+    // );
+
+    //   const bytes = await imageProduct.arrayBuffer();
+    //   const buffer = Buffer.from(bytes);
+
+    const logoPath = path.join(
+      process.cwd(),
+      "public/images/tar",
+      `${timestamp}_${randomString}.png`
+    );
+    await writeFile(logoPath, buffer);
+    //   pathImg = `/images/products/${imageProduct.name}`;
+
+    return NextResponse.json(
+      { imageName: `${timestamp}_${randomString}.png` },
+      { status: 200 }
+    );
+    // return NextResponse.json({ product }, { status: 200 });
+  } catch (error) {
+    console.log(error);
+
+    return NextResponse.json(
+      { message: "Internal Error", error },
+      { status: 500 }
+    );
+  }
 }
-// export async function POST(request) {
-//   console.log(request);
-//   try {
-//     const data = await request.json();
-//     // console.log(data);
-//     const timestamp = Date.now();
-//     const randomString = Math.random().toString(36).substring(2, 18);
-
-//     const base64Data = await data.replace(/^data:image\/png;base64,/, "");
-//     const buffer = Buffer.from(base64Data, "base64");
-//     // fs.writeFileSync(
-//     //   `public/images/tar/${timestamp}_${randomString}.png`,
-//     //   base64Data,
-//     //   "base64"
-//     // );
-
-//     //   const bytes = await imageProduct.arrayBuffer();
-//     //   const buffer = Buffer.from(bytes);
-
-//     const logoPath = path.join(
-//       process.cwd(),
-//       "public/images/tar/",
-//       `${timestamp}_${randomString}.png`
-//     );
-//     await writeFile(logoPath, buffer);
-//     //   pathImg = `/images/products/${imageProduct.name}`;
-
-//     return NextResponse.json(
-//       { imageName: `${timestamp}_${randomString}.png` },
-//       { status: 200 }
-//     );
-//     // return NextResponse.json({ product }, { status: 200 });
-//   } catch (error) {
-//     console.log(error);
-
-//     return NextResponse.json(
-//       { message: "Internal Error", error },
-//       { status: 500 }
-//     );
-//   }
-// }
